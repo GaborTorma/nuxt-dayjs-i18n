8efebb6c03293453c7ee0cb07c7bad60eb5a8dd8
Torma Gábor <tgc@telemedia.hu>
2023-09-01 16:16:21 +0200

    fix(modules): check loaded modules

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -5,6 +5,7 @@ export default defineNuxtPlugin({
 	name: 'nuxt-dayjs-i18n-plugin',
 	// enforce: 'pre', // or 'post'
 	setup(nuxtApp) {
+		console.log('plugin loaded')
 		nuxtApp.hook('i18n:beforeLocaleSwitch', ({ newLocale }) => {
 			const { $dayjs: dayjs } = useNuxtApp()
 			dayjs.locale(newLocale)
@@ -14,7 +15,13 @@ export default defineNuxtPlugin({
 		// You can directly register Nuxt app hooks here
 		'app:created'() {
 			const nuxtApp = useNuxtApp()
-			const dayjs = nuxtApp.$dayjs
+			const { $dayjs: dayjs, $i18n: i18n } = nuxtApp
+			if (!dayjs) {
+				throw new Error('nuxt-dayjs module not found')
+			}
+			if (!i18n) {
+				throw new Error('@nuxtjs/i18n module not found')
+			}
 			const { locale } = nuxtApp.$i18n
 			if (locale.value !== dayjs.locale()) {
 				dayjs.locale(locale.value)
