67f579b327c1b38cca89c0de36850231ebef55bf
Gábor Torma <gabor@torma.dev>
2024-03-28 08:46:19 +0100

    chore: upgrade all deps.

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
 import antfu from '@gabortorma/antfu-eslint-config'
 import withNuxt from './playground/.nuxt/eslint.config.mjs'
 
-export default withNuxt(antfu({}))
+export default withNuxt(antfu())
